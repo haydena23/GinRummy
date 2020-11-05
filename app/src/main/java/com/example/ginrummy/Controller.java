91072dd92a49d7b67efc002366490ab64eec4920
Aron <manalang23@up.edu>
2020-11-04 18:28:16 -1000

    Fixerino

---
+++ app/src/main/java/com/example/ginrummy/Controller.java
@@ -0,0 +1,19 @@
+package com.example.ginrummy;
+
+import android.view.View;
+
+public class Controller implements View.OnClickListener{
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.discardButton:
+                break;
+            case R.id.knockButton:
+                break;
+            case R.id.drawPile:
+                break;
+            case R.id.discardedCard:
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/ginrummy/MainActivity.java
+++ app/src/main/java/com/example/ginrummy/MainActivity.java
@@ -2,35 +2,51 @@ package com.example.ginrummy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_screen);
 
         RummyGameState player = new RummyGameState();
         RummyGameState dumbAI = new RummyGameState(player);
-        ControllerClass controller = new ControllerClass(player);
+
+        Controller controller = new Controller();
 
         /** Assigning button listeners */
         Button quitButton = findViewById(R.id.quitButton);
-        quitButton.setOnClickListener(controller);
-
-        Button doneButton = findViewById(R.id.doneButton);
-        quitButton.setOnClickListener(controller);
-
-        Button pullButton = findViewById(R.id.pullButton);
-        quitButton.setOnClickListener(controller);
-
-        Button discardButton = findViewById(R.id.discardButton);
-        quitButton.setOnClickListener(controller);
+        quitButton.setOnClickListener(this);
 
         Button knockButton = findViewById(R.id.knockButton);
-        quitButton.setOnClickListener(controller);
+        knockButton.setOnClickListener(this);
+
+        Button knock = findViewById(R.id.knockButton);
+
+
+        ImageButton discardButton = findViewById(R.id.discardedCard);
+        discardButton.setOnClickListener(controller);
+
+        ImageButton drawPile = findViewById(R.id.drawPile);
+        drawPile.setOnClickListener(controller);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.startButton:
+                setContentView(R.layout.activity_main);
+                //Some kind of code to make new instance of gameState
+            case R.id.quitButton:
+                setContentView(R.layout.start_screen);
+                break;
+        }
     }
 }
