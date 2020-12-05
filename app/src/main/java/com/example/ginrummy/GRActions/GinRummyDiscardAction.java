53ed698dfd175b0efa81745efbe6a8d148f2f4b6
Aron <manalang23@up.edu>
2020-12-04 18:46:03 -1000

    Action comment headers

--- app/src/main/java/com/example/ginrummy/GRActions/GinRummyDiscardAction.java
+++ app/src/main/java/com/example/ginrummy/GRActions/GinRummyDiscardAction.java
@@ -1,3 +1,12 @@
+/**
+ * GinRummyGameState.java - Class for the sole purpose
+ * of sending an action to the local class, which checks which action it was
+ * and in this case, its a discard action.
+ *
+ * @author Jarren Calizo, Tony Hayden, Aron Manalang, Audrey Sauter
+ * @version 25 Nov 2020
+ */
+
 package com.example.ginrummy.GRActions;
 
 import com.example.game.GameFramework.GamePlayer;
